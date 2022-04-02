b1b97fdecf039a81999700c553f90ef9dd781e5e
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-04-02 15:31:22 +1100

    Adding qualifier examples

--- src/test/java/guru/framework/sfgdi/controllers/SetterInjectedControllerTest.java
+++ src/test/java/guru/framework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -1,10 +1,8 @@
 package guru.framework.sfgdi.controllers;
 
-import guru.framework.sfgdi.services.GreetingServiceImpl;
+import guru.framework.sfgdi.services.ConstructorGreetingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class SetterInjectedControllerTest {
 
@@ -13,7 +11,7 @@ class SetterInjectedControllerTest {
     @BeforeEach
     void setUp() {
         controller = new SetterInjectedController();
-        controller.setGreetingService(new GreetingServiceImpl());
+        controller.setGreetingService(new ConstructorGreetingService());
     }
 
     @Test

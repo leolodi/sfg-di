4f57a596fe13534f2e9d27dc84678aa824624985
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-04-02 15:22:49 +1100

    Adding spring DI examples

--- src/main/java/guru/framework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/framework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,13 @@
 package guru.framework.sfgdi.controllers;
 
 import guru.framework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
 
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting(){
--- src/main/java/guru/framework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/framework/sfgdi/controllers/SetterInjectedController.java
@@ -1,11 +1,15 @@
 package guru.framework.sfgdi.controllers;
 
 import guru.framework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }

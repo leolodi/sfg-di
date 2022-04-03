9fb47fe5ebe8f34d4730883d5def3b05271b2b16
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-04-03 16:27:24 +1000

    Adding examples for Spring Profiles

--- src/main/java/guru/framework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/framework/sfgdi/SfgDiApplication.java
@@ -1,9 +1,6 @@
 package guru.framework.sfgdi;
 
-import guru.framework.sfgdi.controllers.ConstructorInjectedController;
-import guru.framework.sfgdi.controllers.MyController;
-import guru.framework.sfgdi.controllers.PropertyInjectedController;
-import guru.framework.sfgdi.controllers.SetterInjectedController;
+import guru.framework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +10,9 @@ public class SfgDiApplication {
 
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
+		System.out.println(i18nController.sayHello());
 
 		MyController myController = (MyController) ctx.getBean("myController");
 

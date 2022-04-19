409d4c113aee256db305c492916a52e5e7240d78
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-04-19 12:47:29 +1000

    Adding factory bean example

--- src/main/java/guru/framework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/framework/sfgdi/config/GreetingServiceConfig.java
@@ -1,5 +1,7 @@
 package guru.framework.sfgdi.config;
 
+import com.framewok.pets.PetService;
+import com.framewok.pets.PetServiceFactory;
 import guru.framework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.framework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.framework.sfgdi.services.*;
@@ -10,6 +12,23 @@ import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory() {
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Profile({"cat"})
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory) {
+        return petServiceFactory.getPetService("cat");
+    }
 
     @Profile({"ES", "default"})
     @Bean("i18nService")

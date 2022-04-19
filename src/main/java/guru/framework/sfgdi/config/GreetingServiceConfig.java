dcdd15246d7cb393d6ef52d9216dd8eb03a03ad9
Leonardo Lodi <leonardo.tlodi@gmail.com>
2022-04-19 11:35:33 +1000

    Adding java config example

---
+++ src/main/java/guru/framework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package guru.framework.sfgdi.config;
+
+import guru.framework.sfgdi.services.ConstructorGreetingService;
+import guru.framework.sfgdi.services.PropertyInjectedGreetingService;
+import guru.framework.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService() {
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService() {
+        return new SetterInjectedGreetingService();
+    }
+}
--- src/main/java/guru/framework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/framework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -2,7 +2,6 @@ package guru.framework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
 public class PropertyInjectedGreetingService implements GreetingService{
 
     @Override
